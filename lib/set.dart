06302d270cf22ac8db597e45a4c47d93ad76bb35
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-07-09 15:08:03 +0700

    add new set.dart file

---
+++ lib/set.dart
@@ -0,0 +1,15 @@
+void main() {
+  Set<int> numbers = {};
+  var names = <String>{};
+  var doubles = <double>{};
+
+  names.add('Iqbal');
+  names.add('Rasetio');
+  print(names);
+  print(names.length);
+
+  //deklarasi langsung
+  var nama = <String>{'Iqbal', 'Rasetio', 'Azka', 'Salsabila'};
+
+  print(nama);
+}
