8aa13fb8fbf67fa1f5dbbb5707295b106d172f04
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-07-12 11:23:15 +0700

    add new null_safety.dart file

---
+++ lib/null_safety.dart
@@ -0,0 +1,23 @@
+void main() {
+  //null check
+  int? age = null;
+  if (age != null) {
+    double ageDouble = age.toDouble();
+    print(ageDouble);
+  }
+  //konversi non nullable to nullable
+  String name = 'iqbal';
+  String? nullableName = name;
+
+  //konversi nullable to non nullable (harus lakukan null check terlebih dahulu)
+  int? nullablePrice = null;
+  if (nullablePrice != null) {
+    int price = nullablePrice;
+    print(price);
+  }
+
+  //default value pakai ??
+  String? guest;
+  String guestName = guest ?? 'Guest';
+  print(guestName);
+}
