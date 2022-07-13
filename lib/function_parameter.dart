b4a962e1b79bbb637610fcd448954074669da188
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-07-13 13:21:16 +0700

    add new function_parameter.dart file

---
+++ lib/function_parameter.dart
@@ -0,0 +1,8 @@
+//function parameter
+void sayHelloWithParameter(String firstName, String lastName) {
+  print('Hello $firstName $lastName');
+}
+
+void main() {
+  sayHelloWithParameter('Dicki', 'Prasetya');
+}
