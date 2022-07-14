2ac1710eeb7310e27467c2721612fe4eced800b8
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-07-14 11:02:12 +0700

    add new function_return_value.dart file

---
+++ lib/function_return_value.dart
@@ -0,0 +1,18 @@
+String sayHello(String name) {
+  return 'Hello $name';
+}
+
+int sum(List<int> numbers) {
+  var total = 0;
+  for (var value in numbers) {
+    total += value;
+  }
+  return total;
+}
+
+void main() {
+  var data = sayHello('Diki');
+  print(data);
+  var total = sum([10, 10, 10, 10]);
+  print(total);
+}
